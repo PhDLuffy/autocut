0c77308844950ef71a9285141fab4017d428830b
PhDLuffy <PhDLuffy@phdluffy.com>
2022-12-08 17:35:20 +0800

    在whisper基础上构建autocut

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ requirements = [
     "srt",
     "moviepy",
     "opencc-python-reimplemented",
-    "torchaudio",
     "parameterized",
-    "whisper @ git+https://github.com/openai/whisper.git",
 ]
 
 
